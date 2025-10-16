bcbcb26dcf0458dc045be068b084ccfb912314b5
victor alvarez rojas <victoralvarezrojas@Mac-Studio.local>
2025-10-15 23:56:28 -0300

    nuevo ejemplo add manejo de formularios

---
+++ form-handling/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ButtonRow from "./components/buttons/ButtonRow.vue";
+</script>
+
+<template>
+  <div class="title">
+    <h1>FORM - HANDLING</h1>
+  </div>
+  <div class="buttons">
+    <ButtonRow />
+  </div>
+</template>
+
+<style scoped>
+
+
+.title{
+  margin: 0 auto;
+  text-align: center;
+}
+
+.buttons {
+  margin: 2rem;
+}
+</style>
---
+++ form-handling/src/components/buttons/ButtonRow.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const onButtonClick = (evt: Event) => {
+  const button = evt.target as HTMLButtonElement;
+  console.log(`The user clicked ${button.name}: ${button.value}`);
+};
+</script>
+
+<template>
+  <div class="button-list">
+    <button
+      @click="onButtonClick"
+      name="button-hoodie"
+      value="fullstack-hoodie"
+      class="ui button"
+    >
+      Hoodie
+    </button>
+    <button
+      @click="onButtonClick"
+      name="button-tee"
+      value="fullstack-tee"
+      class="ui button"
+    >
+      Tee
+    </button>
+    <button
+      @click="onButtonClick"
+      name="button-fitted-cap"
+      value="fullstack-fitted-cap"
+      class="ui button"
+    >
+      Fitted Cap
+    </button>
+    <button
+      @click="onButtonClick"
+      name="button-jacket"
+      value="fullstack-jacket"
+      class="ui button"
+    >
+      Jacket
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.button-list {
+  display: flex;
+  gap: 1rem;
+}
+</style>
